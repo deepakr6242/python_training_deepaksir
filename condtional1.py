3cb2b9da5cf5f0d3a8a1da7d4edd3ead04e4105c
deepakr6242 <deepakr6242@gmail.com>
2020-12-01 03:58:19 +0000

    Update condtional1.py
    Review Done

--- condtional1.py
+++ condtional1.py
@@ -1,20 +1,37 @@
+
+# marks = int(input('Enter the marks: '))
+
+# if 70 < marks < 80:
+#     print('check 70-75 category and 75-80 category ')
+#     if 70 < marks < 75:
+#         print('Average')
+#     else:
+#         print('above average')
+# elif 75 < marks < 80:
+#     print('above average')
+# elif 80 < marks < 90:
+#     print('Distinction')
+# elif 90 < marks < 100:
+#     print('elite')
+# else:
+    # print('poor students')
+
+
+# to find the type of students based on marks--
+# Reviewed below
 marks = int(input('Enter the marks: '))
 
-if 70 < marks < 80:
-    print('check 70-75 category and 75-80 category ')
-    if 70 < marks < 75:
-        print('Average')
-    else:
-        print('above average')
-elif 75 < marks < 80:
-    print('above average')
-elif 80 < marks < 90:
-    print('Distinction')
-elif 90 < marks < 100:
-    print('elite')
+if 70 <= marks < 80:
+	    print('checking 70-75 category and 75-80 category....... ')
+	    if 70 <= marks <= 75:
+	        print('Average')
+	    else:
+	        print('above average')
+elif 80 <= marks <90:
+	    print('Distinction')
+elif 90 <= marks <=100:
+	    print('elite')
+elif 0 <= marks < 70:
+	    print('Poor students')
 else:
-    print('poor students')
-        
-
-    
-        
+		print('Give valid marks within 100')
